41e1970437c037c64407337275ac031f20e4d153
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-28 12:07:25 +0300

    Update codechallenge.js

--- codechallenge.js
+++ codechallenge.js
@@ -24,3 +24,60 @@ if (speed <= 70) {
 }if (((speed-70)/5)>12) {
   console.log("license suspended");
 }
+
+//Net Salary Calculator
+
+//KRA
+var payee = "";
+{
+  let payee = ("40,000");
+  if (payee <= 24,000) {
+   console.log(rate = "10");
+  } else if (payee >= 24,001 && payee < 32,333) {
+    console.log(rate = "25");
+  } else if (payee > 32,333) {
+    console.log(rate = "30");
+  }
+  }
+
+//NHIF
+  var deduction= "";
+{
+  let Salary = ("100,000");
+  if (Salary >= 100,000) {
+   console.log(deduction = "1,700");
+  } else if (Salary = 90,000 && Salary <= 99,999) {
+    console.log(deduction = "1,600");
+  } else if (Salary = 80,000 && Salary <= 89,999) {
+    console.log(deduction = "1,500");
+  } else if (Salary = 70,000 && Salary <= 79,999) {
+    console.log(deduction = "1,400");
+  } else if (Salary = 60,000  && Salary <= 69,999) {
+    console.log(deduction = "1,300");
+  } else if (Salary = 50,000 && Salary <= 59,999) {
+    console.log(deduction = "1,200");
+  } else if (Salary = 45,000 && Salary <= 49,999) {
+    console.log(deduction = "1,100");
+  } else if (Salary = 40,000 && Salary <= 44,999) {
+    console.log(deduction = "1,000");
+  }else if (Salary = 35,000 && Salary <= 39,999) {
+    console.log(deduction = "950");
+  }else if (Salary = 30,000 && Salary <= 34,999) {
+    console.log(deduction = "900");
+  }else if (Salary = 25,000 && Salary <= 29,999) {
+    console.log(deduction = "850");
+  }else if (Salary = 20,000 && Salary <= 24,999) {
+    console.log(deduction = "750");
+  }else if (Salary = 15,000 && Salary <= 19,999) {
+    console.log(deduction = "600");
+  }else if (Salary = 12,000 && Salary <= 14,999) {
+    console.log(deduction = "500");
+  }else if (Salary = 8,000 && Salary <= 11,999) {
+    console.log(deduction = "400");
+  }else if (Salary = 6,000 && Salary <= 7,999) {
+    console.log(deduction = "300");
+  }else if (Salary <= 5,999) {
+    console.log(deduction = "150");
+}
+
+}
